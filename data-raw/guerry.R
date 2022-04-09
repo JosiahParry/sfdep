849bb94788b8b6da0eb43ca14af465e5cbb8ca3b
josiahparry <josiah.parry@gmail.com>
2022-04-09 10:03:39 -0400

    convert guerry to ascii

--- data-raw/guerry.R
+++ data-raw/guerry.R
@@ -1,9 +1,9 @@
 ## code to prepare `DATASET` dataset goes here
+library(sfdep)
 
 guerry <- sf::st_as_sf(Guerry::gfrance85) |>
   janitor::clean_names() |>
   sf::st_set_crs(27572) |>
-  #sf::st_transform(crs = "EPSG:4326") %>%
   tibble::as_tibble() |>
   sf::st_as_sf()
 
@@ -11,5 +11,8 @@ guerry_nb <- guerry |>
   dplyr::mutate(nb = st_contiguity(geometry),
                 wt = st_weights(nb))
 
+save(guerry, file = "data/guerry.rda", ascii = TRUE, compress = "xz")
+save(guerry_nb, file = "data/guerry_nb.rda", ascii = TRUE, compress = "xz")
 
-usethis::use_data(guerry, guerry_nb, overwrite = TRUE)
+# load("data/guerry.rda")
+# load("data/guerry_nb.rda")
