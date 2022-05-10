2bc3510b8e8fabdf64bd7080293d0698ce8c21bf
josiahparry <josiah.parry@gmail.com>
2022-05-10 11:05:15 -0400

    add progress on emerging hotspots

--- R/emerging-hostpot-classifications.R
+++ R/emerging-hostpot-classifications.R
@@ -9,36 +9,50 @@ new_hotspot <- function(gs, sigs, n, ...) {
 consecutive_hotspot <- function(gs, sigs, n, ...) {
   if (!(sigs[n] && gs[n] > 0)) return(FALSE)
 
-  n_final_run <- which(diff(cumsum(rev(sigs))) == 0)[1]
+  n_final_run <- which(diff(cumsum(c(rev(sigs), FALSE))) == 0)[1]
   run_index <- seq(n-n_final_run + 1, n, by = 1)
 
   all(!sigs[-run_index]) &&
     all(sigs[run_index]) &&
     all(!((sigs[-run_index]) & (gs[-run_index] > 0))) &&
     all(!sigs[-run_index]) &&
-    (sum(sigs & gs > 0) / n <= 0.9)
+    (sum(sigs & gs > 0) / n < 0.9)
 }
 
-intensifying_hotspot <- function(gs, sigs, tau, tau_p, threshold, ...) {
-  (sum(sigs) / n  >= .9) && (sigs[n]) && (tau > 0) && (tau_p <= threshold)
+intensifying_hotspot <- function(gs, sigs, n, tau, tau_p, threshold, ...) {
+  (sum(sigs) / n  >= .9) && (sigs[n]) && (tau > 0) && (tau_p < threshold)
 }
 
-persistent_hotspot <- function(gs, sigs, tau_p, threshold, ...) {
+persistent_hotspot <- function(gs, sigs, tau_p, threshold, n, ...) {
   (sum(sigs & gs > 0) / n  >= .9) && (tau_p > threshold)
 }
 
-diminishing_hotspot <- function(gs, sigs, tau, tau_p, threshold, ...) {
+diminishing_hotspot <- function(gs, sigs, tau, tau_p, threshold, n, ...) {
   (sum(sigs & gs > 0) / n  >= .9) &&
     (sigs[n]) && (tau < 0) &&
     (tau_p <= threshold)
 }
 
 sporadic_hotspot <- function(gs, sigs, n, ...) {
-  (sum(gs > 0 & sigs) / n <= 0.9) && !any(gs < 0 & sigs)
+  # A location that is an on-again then off-again hot spot.
+  # Less than ninety percent of the time-step intervals have been statistically
+  # significant hot spots and none of the time-step intervals have been
+  # statistically significant cold spots.
+  # JP 2022-05-09: missing condition here that i think is implicit that
+  #   there needs to be _at least_ 1 significant hotspot
+  (sum(gs > 0 & sigs) / n < 0.9) &&
+    # no sig cold spots
+    !any(gs < 0 & sigs) &&
+    # at least one hot spot
+    any(gs > 0 & sigs)
 }
 
 sporadic_coldspot <- function(gs, sigs, n, ...) {
-  (sum(gs < 0 & sigs) / n <= 0.9) && !any(gs > 0 & sigs)
+  (sum(gs < 0 & sigs) / n < 0.9) &&
+    # not sig hot spots
+    !any(gs > 0 & sigs) &&
+    # at least 1 cold spot
+    any(gs < 0 & sigs)
 }
 
 
@@ -55,19 +69,24 @@ new_coldspot <- function(gs, sigs, n, ...) {
 }
 
 consecutive_coldspot <- function(gs, sigs, n, ...) {
+  # A location with a single uninterrupted run of statistically significant
+  # cold spot bins in the final time-step intervals. The location has never been
+  # a statistically significant cold spot prior to the final cold spot run and
+  # less than ninety percent of all bins are statistically significant cold spots
   if (!(sigs[n] && gs[n] < 0)) return(FALSE)
 
-  n_final_run <- which(diff(cumsum(rev(sigs))) == 0)[1]
+  n_final_run <- which(diff(cumsum(c(rev(sigs), FALSE))) == 0)[1]
   run_index <- seq(n-n_final_run + 1, n, by = 1)
 
   all(!sigs[-run_index]) &&
     all(sigs[run_index]) &&
     all(!((sigs[-run_index]) & (gs[-run_index] > 0))) &&
     all(!sigs[-run_index]) &&
-    (sum(sigs & gs > 0) / n <= 0.9)
+    (sum(sigs & gs < 0) / n < 0.9)
 }
 
-intensifying_coldspot <- function(gs, sigs, n, tau, tau_p, threshold, ...){
+intensifying_coldspot <- function(gs, sigs, n, tau, tau_p,
+                                  threshold, ...){
   (sum(sigs & gs < 0) / n  >= .9) &&
     (sigs[n]) &&
     (tau < 0) &&
@@ -93,34 +112,37 @@ historical_coldspot <- function(gs, sigs, n, ...) {
 fxs <- list(
   "new_hotspot" = new_hotspot,
   "new_coldspot" = new_coldspot,
-  "consecutive_hotspot" = consecutive_hotspot,
-  "consecutive_coldspot" = consecutive_coldspot,
-  "intensifying_hotspot" = intensifying_hotspot,
-  "intensifying_coldspot" = intensifying_coldspot,
-  "persistent_hotspot" = persistent_hotspot,
-  "persistent_coldspot" = persistent_coldspot,
-  "diminishing_hotspot" = diminishing_hotspot,
-  "diminishing_coldspot" = diminishing_coldspot,
-  "sporadic_hotspot" = sporadic_hotspot,
-  "sporadic_coldspot" = sporadic_coldspot,
-  "oscilating_hotspot" = oscilating_hotspot,
-  "oscilating_coldspot" = oscilating_coldspot,
-  "historical_hotspot" = historical_hotspot,
-  "historical_coldspot" = historical_coldspot)
-
-.x <- splits[[1]]
-threshold = 0.05
-
-test_res <- Kendall::MannKendall(gs)
-tau = test_res[["tau"]]
-tau_p = as.numeric(test_res[["sl"]])
-
-sigs = .x[["p_sim"]] <= threshold
-gs = .x[["gi_star"]]
-n = length(gs)
+  "consecutive hotspot" = consecutive_hotspot,
+  "consecutive coldspot" = consecutive_coldspot,
+  "intensifying hotspot" = intensifying_hotspot,
+  "intensifying coldspot" = intensifying_coldspot,
+  "persistent hotspot" = persistent_hotspot,
+  "persistent coldspot" = persistent_coldspot,
+  "diminishing hotspot" = diminishing_hotspot,
+  "diminishing coldspot" = diminishing_coldspot,
+  "oscilating hotspot" = oscilating_hotspot,
+  "oscilating coldspot" = oscilating_coldspot,
+  "historical hotspot" = historical_hotspot,
+  "historical coldspot" = historical_coldspot,
+  "sporadic hotspot" = sporadic_hotspot,
+  "sporadic coldspot" = sporadic_coldspot,
+  "no pattern detected" = function(...) TRUE)
 
 
-classify_hotspot <- function(.x, mk_test, threshold) {
+
+#' Classify Hot Spot results
+#'
+#' Given the Gi* time-series and Mann Kendall scores classify the hotspot values
+#'
+classify_hotspot <- function(.x, threshold) {
+  gs = .x[["gi_star"]]
+  sigs = .x[["p_sim"]] <= threshold
+  n = length(gs)
+  mktest <- Kendall::MannKendall(gs)
+  tau = mktest[["tau"]]
+  tau_p = as.numeric(mktest[["sl"]])
+
+
   res <- lapply(fxs, function(.x, ...) .x(...),
                 gs = gs,
                 sigs = sigs,
@@ -129,97 +151,8 @@ classify_hotspot <- function(.x, mk_test, threshold) {
                 tau_p = tau_p,
                 threshold = threshold
   )
-  names(res[unlist(res)])
+  cbind(as.data.frame(unclass(mktest)),
+        classification = as.character(names(res[unlist(res)])[1])
+        )
+
 }
-
-classify_hotspot(.x)
-res <- lapply(fxs, function(.x, ...) .x(...),
-       gs = gs,
-       sigs = sigs,
-       n = n,
-       tau = tau,
-       tau_p = tau_p,
-       threshold = threshold
-       )
-
-
-names(res[unlist(res)])
-
-# "New Hot Spot
-#
-# A location that is a statistically significant hot spot for the final time step and has never been a statistically significant hot spot before.
-#
-# Consecutive Hot Spot
-# Consecutive Hot Spot
-#
-# A location with a single uninterrupted run of statistically significant hot spot bins in the final time-step intervals. The location has never been a statistically significant hot spot prior to the final hot spot run and less than ninety percent of all bins are statistically significant hot spots.
-#
-# Intensifying Hot Spot
-# Intensifying Hot Spot
-#
-# A location that has been a statistically significant hot spot for ninety percent of the time-step intervals, including the final time step. In addition, the intensity of clustering of high counts in each time step is increasing overall and that increase is statistically significant.
-#
-# Persistent Hot Spot
-# Persistent Hot Spot
-#
-# A location that has been a statistically significant hot spot for ninety percent of the time-step intervals with no discernible trend indicating an increase or decrease in the intensity of clustering over time.
-#
-# Diminishing Hot Spot
-# Diminishing Hot Spot
-#
-# A location that has been a statistically significant hot spot for ninety percent of the time-step intervals, including the final time step. In addition, the intensity of clustering in each time step is decreasing overall and that decrease is statistically significant.
-#
-# Sporadic Hot Spot
-# Sporadic Hot Spot
-#
-# A location that is an on-again then off-again hot spot. Less than ninety percent of the time-step intervals have been statistically significant hot spots and none of the time-step intervals have been statistically significant cold spots.
-#
-# Oscillating Hot Spot
-# Oscillating Hot Spot
-#
-# A statistically significant hot spot for the final time-step interval that has a history of also being a statistically significant cold spot during a prior time step. Less than ninety percent of the time-step intervals have been statistically significant hot spots.
-#
-# Historical Hot Spot
-# Historical Hot Spot
-#
-# The most recent time period is not hot, but at least ninety percent of the time-step intervals have been statistically significant hot spots.
-#
-# New Cold Spot
-# New Cold Spot
-#
-# A location that is a statistically significant cold spot for the final time step and has never been a statistically significant cold spot before.
-#
-# Consecutive Cold Spot
-# Consecutive Cold Spot
-#
-# A location with a single uninterrupted run of statistically significant cold spot bins in the final time-step intervals. The location has never been a statistically significant cold spot prior to the final cold spot run and less than ninety percent of all bins are statistically significant cold spots.
-#
-# Intensifying Cold Spot
-# Intensifying Cold Spot
-#
-# A location that has been a statistically significant cold spot for ninety percent of the time-step intervals, including the final time step. In addition, the intensity of clustering of low counts in each time step is increasing overall and that increase is statistically significant.
-#
-# Persistent Cold Spot
-# Persistent Cold Spot
-#
-# A location that has been a statistically significant cold spot for ninety percent of the time-step intervals with no discernible trend, indicating an increase or decrease in the intensity of clustering of counts over time.
-#
-# Diminishing Cold Spot
-# Diminishing Cold Spot
-#
-# A location that has been a statistically significant cold spot for ninety percent of the time-step intervals, including the final time step. In addition, the intensity of clustering of low counts in each time step is decreasing overall and that decrease is statistically significant.
-#
-# Sporadic Cold Spot
-# Sporadic Cold Spot
-#
-# A location that is an on-again then off-again cold spot. Less than ninety percent of the time-step intervals have been statistically significant cold spots and none of the time-step intervals have been statistically significant hot spots.
-#
-# Oscillating Cold Spot
-# Oscillating Cold Spot
-#
-# A statistically significant cold spot for the final time-step interval that has a history of also being a statistically significant hot spot during a prior time step. Less than ninety percent of the time-step intervals have been statistically significant cold spots.
-#
-# Historical Cold Spot
-# Historical Cold Spot
-#
-# The most recent time period is not cold, but at least ninety percent of the time-step intervals have been statistically significant cold spots."
--- dev/emerging-dev.R
+++ dev/emerging-dev.R
@@ -22,8 +22,7 @@ regions <- select(x, .region_id = CT_ID_10)
 # Identify regions with only missing values -------------------------------
 # Creates objects complete_geo and complete_obs
 # maybe should create a threshold fore completeness? like 75%+ completeness
-splits <- split(guns, guns$.region_id)
-
+splits <- split(guns, guns[[".region_id"]])
 incompletes <- do.call(rbind, lapply(splits, function(.x) {
   sum(is.na(.x[["value"]])) == length(.x[["value"]])
 }))
@@ -41,16 +40,16 @@ rm(splits)
 
 # cast to spacetime
 .var = "value"
+nsim = 199
 
 spt <- spacetime(complete_obs, complete_geo, ".region_id", "time_period")
 
 x <- spt
 # function dev ------------------------------------------------------------
 
-#' TODO arguments to add
-#' distance band, for distance weights
-#' default uses queen contiguity
-#' return final calculation, intermediate (each time period), or both
+#' TODO incorporate time-lagged neighbors
+#' TODO arguments to add own neighbors and wieghts
+#' TODO arg for return final calculation, intermediate (each time period), or both
 emerging_hotspot_analysis <- function(x, var, nsim = 199, ...) {
   if (!is_spacetime_cube(x)) cli::cli_abort("`x` is not a spacetime cube.")
   if (active(x) == "geometry") x <- activate(x, "data")
@@ -73,10 +72,10 @@ emerging_hotspot_analysis <- function(x, var, nsim = 199, ...) {
                time_period = .x[[.time_col]])
   })
 
+  x <- do.call(rbind, all_gis)
   # Bind together
   trend_res <- do.call(rbind,
-          lapply(split(do.call(rbind, all_gis),
-                       x[[.loc_col]]), function(.x) {
+          lapply(split(x, x[[.loc_col]]), function(.x) {
                          data.frame(
                            unclass(Kendall::MannKendall(.x[["gi_star"]]))
                          )
@@ -84,7 +83,9 @@ emerging_hotspot_analysis <- function(x, var, nsim = 199, ...) {
 }
 
 
-splits <- split(do.call(rbind, all_gis), x[[.loc_col]])
+df <- do.call(rbind, all_gis)
+
+splits <- split(x, x[[.loc_col]])
 
 
 xps <- do.call(rbind, all_gis) |>
@@ -97,92 +98,6 @@ ps <= 0.05
 
 trend_res
 threshold = 0.05
-
-# Hotspot clssification ---------------------------------------------------
-# https://pro.arcgis.com/en/pro-app/2.8/tool-reference/space-time-pattern-mining/learnmoreemerging.htm#GUID-09587AFC-F5EC-4AEB-BE8F-0E0A26AB9230
-
-#' New Hot Spot : A location that is a statistically significant hot spot for the final time step and has never been a statistically significant hot spot before.
-
-gs <- c(0, 0.15, 0.7, 1.5)
-ps <- c(0.7, 0.5, 0.6, 0.01)
-sigs <- ps <= threshold
-n <- length(gs)
-
-# condition for new hot spot
-(sum(sigs) == 1) && sigs[n] && gs[n] > 0
-
-
-
-# Intensifying Hot Spot ---------------------------------------------------
-#
-# A location that has been a statistically significant hot spot for ninety percent
-# of the time-step intervals, including the final time step. In addition, the intensity of clustering of high counts in each time step is increasing overall and that increase is statistically significant.
-gs <- seq(0, 2, length.out = 15)
-ps <- seq(0.051, 0.00000001, length.out = 15)
-sigs <- ps <= threshold
-n <- length(gs)
-first_obs <- n - floor(n * 0.9)
-
-# check that 90% of spots have been increasing
-sum(sigs) / n  >= .9
-
-# check last spot is significant
-sigs[n]
-
-# check that intensity is increasing and is significant for 90% and increasing
-all(sigs[first_obs:n]) & !is.unsorted(gs[first_obs:n])
-
-# Persistent Hot Spot
-#
-# A location that has been a statistically significant hot spot for ninety percent of the time-step intervals with no discernible trend indicating an increase or decrease in the intensity of clustering over time.
-#
-# Diminishing Hot Spot
-#
-# A location that has been a statistically significant hot spot for ninety percent of the time-step intervals, including the final time step. In addition, the intensity of clustering in each time step is decreasing overall and that decrease is statistically significant.
-#
-# Sporadic Hot Spot
-#
-# A location that is an on-again then off-again hot spot. Less than ninety percent of the time-step intervals have been statistically significant hot spots and none of the time-step intervals have been statistically significant cold spots.
-#
-# Oscillating Hot Spot
-#
-# A statistically significant hot spot for the final time-step interval that has a history of also being a statistically significant cold spot during a prior time step. Less than ninety percent of the time-step intervals have been statistically significant hot spots.
-#
-# Historical Hot Spot
-#
-# The most recent time period is not hot, but at least ninety percent of the time-step intervals have been statistically significant hot spots.
-#
-# New Cold Spot
-#
-# A location that is a statistically significant cold spot for the final time step and has never been a statistically significant cold spot before.
-#
-# Consecutive Cold Spot
-#
-# A location with a single uninterrupted run of statistically significant cold spot bins in the final time-step intervals. The location has never been a statistically significant cold spot prior to the final cold spot run and less than ninety percent of all bins are statistically significant cold spots.
-#
-# Intensifying Cold Spot
-#
-# A location that has been a statistically significant cold spot for ninety percent of the time-step intervals, including the final time step. In addition, the intensity of clustering of low counts in each time step is increasing overall and that increase is statistically significant.
-#
-# Persistent Cold Spot
-#
-# A location that has been a statistically significant cold spot for ninety percent of the time-step intervals with no discernible trend, indicating an increase or decrease in the intensity of clustering of counts over time.
-#
-# Diminishing Cold Spot
-#
-# A location that has been a statistically significant cold spot for ninety percent of the time-step intervals, including the final time step. In addition, the intensity of clustering of low counts in each time step is decreasing overall and that decrease is statistically significant.
-#
-# Sporadic Cold Spot
-#
-# A location that is an on-again then off-again cold spot. Less than ninety percent of the time-step intervals have been statistically significant cold spots and none of the time-step intervals have been statistically significant hot spots.
-#
-# Oscillating Cold Spot
-#
-# A statistically significant cold spot for the final time-step interval that has a history of also being a statistically significant hot spot during a prior time step. Less than ninety percent of the time-step intervals have been statistically significant cold spots.
-#
-# Historical Cold Spot
-#
-# The most recent time period is not cold, but at least ninety percent of the time-step intervals have been statistically significant cold spots.
 
 
 
