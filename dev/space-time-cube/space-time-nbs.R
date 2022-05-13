1b0ae0f8130ba7dc8fc5249751f271c33a625ea6
josiahparry <josiah.parry@gmail.com>
2022-05-13 11:14:39 -0400

    implement spacetime local G. Its slow as with all code i write...

--- dev/space-time-cube/space-time-nbs.R
+++ dev/space-time-cube/space-time-nbs.R
@@ -9,8 +9,12 @@ xnb <- x |>
 
 
 .loc_col <- attr(xnb, "loc_col")
-n_locs <- length(attr(xnb, "geometry")[[.loc_col]])
-n_times <- length(attr(xnb, "times"))
+.time_col <- attr(xnb, "time_col")
+
+# identify number of unique locations and times
+# th3ese should be automatically stored in attributes and updated with [.spacetime method
+n_locs <- length(unique(activate(xnb, "data")[[.loc_col]]))
+n_times <- length(unique(activate(xnb, "data")[[.time_col]]))
 
 nb <- xnb[["nb"]]
 wt <- xnb[["wt"]]
@@ -27,12 +31,12 @@ k = 1
 # If k = 1 then all nbs for the lag at t = 1 are missing
 # Then all nbs at t = 2 are those from t = 1
 
-# Get original time index for a single observation
+# Get original time index for a single time-slice
 nb_time_index_obs <- function(time_index, n_locs, nb) {
   lapply(nb, function(x) x + n_locs * (time_index - 1))
 }
 
-# Get original time index for all observations
+# Get original time index for all time-slices
 nb_time_index <- function(n_times, n_locs, nb) {
   unlist(
     lapply(1:n_times, nb_time_index_obs, n_locs, nb),
@@ -40,8 +44,9 @@ nb_time_index <- function(n_times, n_locs, nb) {
     )
 }
 
+# Get t = 0 simeslice indexes
+# nbt0 = neighbors time 0
 nbt0 <- nb_time_index(n_times, n_locs, nb)
-
 
 # Given the original time-index, identify the lagged time
 # TODO: do this for more than one lag period
@@ -53,48 +58,53 @@ nb_time_index_lag <- function(nbt, n_locs, k) {
 }
 
 # identify time lag for k
-nbt1 <- nb_time_index_lag(nbt, n_locs, k)
+nbt1 <- nb_time_index_lag(nbt0, n_locs, k)
 
-#   function(time_index, n_locs, nb, k) {
-#   lapply(nb, function(x) {
-#     res <- x + n_locs * (time_index - 1)
-#     res <- res - (k * n_locs)
-#     na.omit(ifelse(res < 1, NA, res))
-#   })
-# }
+# combine all lists element-wise
+zip_lists = function (lists) {
+  do.call('Map', c(`c`, lists))
+}
 
 # these are the original neighbors for t =  we then need to combine
 # with original nbs (t = 0). For the first obs (1:n_locs) they will
 # be missing.
 # card for time-lagged neighbors = card * k
-
-nb_tlag <- Map(c, rep(nb, n_times), nbt)
+nb_tlag <- zip_lists(list(nbt0, nbt1))
 
 # create list for spatial weights
 # must filter out wherever card(nbt) == card(nb)
-wt_tlag <- Map(c, rep(wt, n_times), rep(wt, n_times))
+wt_tlag <- zip_lists(replicate(k + 1, rep(wt, n_times), simplify = FALSE))
 
-wtk <- replicate(k + 1, rep(wt, n_times), simplify = FALSE)
+# for the n_locs * k observations, we select only the first card(nbt)
+# obs to omit weights where lag is 0
+n_omit <- (n_locs*k)
+lag_cards <- lengths(nb_tlag)[1:n_omit]
+
+# iterate through those observations to subset for weights
+for (i in 1:n_omit) {
+  wt_tlag[[i]] <- wtk[[i]][1:(lag_cards[i])]
+}
+
 
 # combine all lists rowwise
 zip_lists = function (lists) {
   do.call('Map', c(`c`, lists))
 }
-
 # combine wts lists
-zip_lists(wtk)
 
 # TODO: remove weights elements from 1:n_loc*k for each time-lag
 
-
 # this will filter out where there was no available lag
-index <- (lengths(nbt0) != lengths(nb_tlag))
+index <- (n_omit + 1):(n_locs * n_times)
 
 # These are the values that we'll use to calculate local Gi*
-all_nbs <- nb_tlag[index]
-all_wts <- wt_tlag[index]
-xvar <- xnb[[.var]][index]
-time_index <- xnb[[.time_col]][index]
+# I cant subset this! Because it messes up the calculation!!!!
+# then the row inderxes get shifted and become meaning lessss
+# UGH FUCK
+all_nbs <- nb_tlag
+all_wts <- wt_tlag
+xvar <- activate(xnb, "data")[[.var]]
+time_index <- activate(xnb, "data")
 
 # Find xj for x and time-lagged xj
 xj <- find_xj(xvar, all_nbs)
@@ -108,17 +118,101 @@ wj <- all_wts[1:168]
 xj <- xj[1:168]
 
 # adapted from the body of spdep::localG
-n <- length(wj)
-xibar <- rep(mean(x), n)
-lx <- mapply(xj, wj, FUN = function(x, y) sum(x * y))
-si2 <- rep(sum(scale(x, scale = FALSE)^2)/n, n)
-Wi <- sapply(wj, sum)
-S1i <- sapply(wj, function(x) sum(x^2))
-EG <- Wi * xibar
-res <- (lx - EG)
-VG <- si2 * ((n * S1i - Wi^2)/(n - 1))
-res <- res/sqrt(VG)
-res
+
+
+
+local_g_spt_calc <- function(x, xj, wj) {
+  # This comes from the body of spdep::localG()
+  n <- length(wj)
+  xibar <- rep(mean(x), n)
+  lx <- mapply(xj, wj, FUN = function(x, y) sum(x * y))
+  si2 <- rep(sum(scale(x, scale = FALSE)^2)/n, n)
+  Wi <- sapply(wj, sum)
+  S1i <- sapply(wj, function(x) sum(x^2))
+  EG <- Wi * xibar
+  res <- (lx - EG)
+  VG <- si2 * ((n * S1i - Wi^2)/(n - 1))
+  res <- res/sqrt(VG)
+  res
+}
+
+# GREAT! Now apply this to every split in the dataset
+# TODO try making a version of this function that uses a for loop based on
+# indexes. That may actually be faster than creating a dataframe, splitting it,
+# lapply, etc etc.
+local_g_spt_impl <- function(x, times, nb, wt) {
+  x <- data.frame(
+    x = x,
+    xj = I(find_xj(x, nb)),
+    nb = I(nb),
+    wt = I(wt),
+    time_period = times
+  )
+
+  # returns a vector
+  unname(unlist(lapply(split(x, x[["time_period"]]),
+           function(.x) {
+              local_g_spt_calc(
+                 .x[["x"]],
+                 .x[["xj"]],
+                 .x[["nb"]])
+           })))
+
+  # returns a data frame
+  # do.call(
+  #   rbind,
+  #   lapply(split(x, x[["time_period"]]),
+  #          function(.x) {
+  #            data.frame(
+  #              gi_star = local_g_spt_calc(
+  #                .x[["x"]],
+  #                .x[["xj"]],
+  #                .x[["nb"]]),
+  #              time_period = .x[["time_period"]]
+  #            )
+  #          })
+  # )
+
+}
+
+
+obs <- local_g_spt_impl(xnb[[.var]],
+                        xnb[[.time_col]],
+                        all_nbs, all_wts)
+
+
+
+reps <- matrix(ncol = 199, nrow = n_locs * n_times)
+
+for (i in 1:nsim) {
+  reps[,i] <- local_g_spt_impl(xnb[[.var]],
+                            xnb[[.time_col]],
+                            cond_permute_nb(all_nbs),
+                            all_wts)
+}
+
+l <- (rowSums(obs >= reps)  + 1)/ (nsim + 1)
+g <- (rowSums(obs <= reps) + 1) / (nsim + 1)
+
+data.frame(
+  gi_star = obs,
+  p_sim = pmin(l, g)
+)
+
+# TODO
+local_g_spt_perm <- function(x, times, nb, wt) {
+  p_nb = cond_permute_nb(nb)
+  #p_wt = find_xj(wt, p_nb)
+  res <- local_g_spt_impl(x, times, p_nb, wt)
+}
+
+
+x <- xnb[["value"]]
+times <- xnb[["time_period"]]
+
+local_g_spt_perm(x, times, nb, wt)
+
+
 # Notes -------------------------------------------------------------------
 
 # nb_time_index <- function(time_index, n_locs, nb) {
@@ -149,4 +243,33 @@ xyz <- xx |>
 
 
 
+#   function(time_index, n_locs, nb, k) {
+#   lapply(nb, function(x) {
+#     res <- x + n_locs * (time_index - 1)
+#     res <- res - (k * n_locs)
+#     na.omit(ifelse(res < 1, NA, res))
+#   })
+# }
 
+
+
+
+
+xyz <- cbind(activate(xnb, "data")[, c("time_period", "value")],
+             nb = I(all_nbs),
+             wt = I(all_wts),
+             xj = I(xj))
+
+all_gis <- do.call(
+  rbind,
+  lapply(split(xyz, xyz[[.time_col]]),
+         function(.x) {
+           data.frame(
+             gi_star = local_g_spt(
+               .x[["value"]],
+               .x[["xj"]],
+               .x[["nb"]]),
+             time_period = .x[[.time_col]]
+           )
+         })
+)
